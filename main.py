196dfd58dfa9625fdfeccb893d3226ca2a5cde3c
Ganesh G Setty <ganeshsetty.04@gmail.com>
2022-10-20 17:54:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from firebase_admin import credentials,firestore
 cred = credentials.Certificate('firebase.json')
 
 # Initialize the app with a service account, granting admin privileges
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://databaseName.firebaseio.com'
-})
+firebase_admin.initialize_app(cred)
 
 # As an admin, the app has access to read and write all data, regradless of Security Rules
 db=firestore.client()
-print(db.collection("user_public").get())
+for i in db.collection("user_public").get():
+    print(i.id, i.to_dict())
