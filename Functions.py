27e5d64094c978ea66e827ecd90f630dd7e9a885
Ganesh G Setty <ganeshsetty.04@gmail.com>
2022-11-18 14:56:41 +0530

    Added timed input and functions

---
+++ Functions.py
@@ -0,0 +1,47 @@
+from __future__ import print_function
+import firebase_admin
+from firebase_admin import credentials,firestore
+from msvcrt import kbhit, getwch
+import time
+import sys
+def newMessages():
+    pass
+    #checkin dbif new
+    #return list of new
+
+def chatlist(new):
+    pass
+    #import all text files 
+    #print indexed files
+    #retrn chatlist without new 
+
+def print_chatlist(new,others):
+    pass
+
+def print_flush(*args):
+    print(*args, end='')
+    sys.stdout.flush()
+    
+def timed_input(prompt='', timeout=None):
+    if timeout is None:
+        return input(prompt)
+    print_flush(prompt)
+    start = time.time()
+    response = ''
+    while time.time() - start < timeout:
+        if kbhit():
+            char = getwch()
+            if char == '\r':
+                break
+            elif char == '\x08': # backspace
+                if response:
+                    print_flush(char, char)
+                    response = response[:-1]
+            else:
+                print_flush(char)
+                response += char
+        time.sleep(0.01)
+    else:
+        response = None
+    print()
+    return response
--- main.py
+++ main.py
@@ -1,12 +1,33 @@
+from __future__ import print_function
 import firebase_admin
 from firebase_admin import credentials,firestore
-
-cred = credentials.Certificate('firebase.json')
-
-# Initialize the app with a service account, granting admin privileges
-firebase_admin.initialize_app(cred)
-
-# As an admin, the app has access to read and write all data, regradless of Security Rules
+from msvcrt import kbhit, getwch
+import time
+import sys
+from Functions import*
+#============firebase================
+cred = credentials.Certificate('firebase.json') # Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred) # As an admin, the app has access to read and write all data, regradless of Security Rules
 db=firestore.client()
 for i in db.collection("user_public").get():
     print(i.id, i.to_dict())
+
+#============load user================
+try:
+    pass
+    #open users stored private key file/img
+    #open('abc.txt')
+except:
+    pass
+    #create and open users stored private key file/img
+    #open('abc.txt')
+
+#============display/functioning================
+
+
+while True:
+    new=newMessages()
+    others=chatList(new)
+    print_chatlist(new, others)
+    inp = timed_input("You have ten seconds to answer!",10)
+    print(inp)
