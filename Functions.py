7e4d71c1cd61eb354ee244ac1be4ad2f349da8c1
Ganesh G Setty <ganeshsetty.04@gmail.com>
2022-11-19 15:28:19 +0530

    Steganography from Ibrahim's branch

--- Functions.py
+++ Functions.py
@@ -4,14 +4,90 @@ from firebase_admin import credentials,firestore
 from msvcrt import kbhit, getwch
 import time
 import sys,os
+from PIL import Image
 
-def EncodeSteg(img,string):
-    pass
-#return encoded image
+def genData(data):
+        newd = []
+ 
+        for i in data:
+            newd.append(format(ord(i), '08b'))
+        return newd
+def modPix(pix, data):
+ 
+    datalist = genData(data)
+    lendata = len(datalist)
+    imdata = iter(pix)
+ 
+    for i in range(lendata):
+        pix = [value for value in imdata.__next__()[:3] +
+                                imdata.__next__()[:3] +
+                                imdata.__next__()[:3]]
+        for j in range(0, 8):
+            if (datalist[i][j] == '0' and pix[j]% 2 != 0):
+                pix[j] -= 1
+ 
+            elif (datalist[i][j] == '1' and pix[j] % 2 == 0):
+                if(pix[j] != 0):
+                    pix[j] -= 1
+                else:
+                    pix[j] += 1
+        if (i == lendata - 1):
+            if (pix[-1] % 2 == 0):
+                if(pix[-1] != 0):
+                    pix[-1] -= 1
+                else:
+                    pix[-1] += 1
+ 
+        else:
+            if (pix[-1] % 2 != 0):
+                pix[-1] -= 1
+ 
+        pix = tuple(pix)
+        yield pix[0:3]
+        yield pix[3:6]
+        yield pix[6:9]
 
-def DecodeSteg(img):
-    pass
-#return decoded string
+def stegEncode(img, data):
+    newimg=img.copy()
+    w = newimg.size[0]
+    (x, y) = (0, 0)
+ 
+    for pixel in modPix(newimg.getdata(), data):
+        newimg.putpixel((x, y), pixel)
+        if (x == w - 1):
+            x = 0
+            y += 1
+        else:
+            x += 1
+    return newimg        
+def stegDecode(img):
+    data = ''
+    imgdata = iter(img.getdata())
+ 
+    while (True):
+        pixels = [value for value in imgdata.__next__()[:3] +
+                                imgdata.__next__()[:3] +
+                                imgdata.__next__()[:3]]
+        binstr = ''
+ 
+        for i in pixels[:8]:
+            if (i % 2 == 0):
+                binstr += '0'
+            else:
+                binstr += '1'
+ 
+        data += chr(int(binstr, 2))
+        if (pixels[-1] % 2 != 0):
+            return data
+def stego_example():
+    img='stego img.jpg';        
+    img = Image.open(img, 'r')
+    text="this needs to be decoded"
+    newimage=encode(img,text)
+    newimage.save("Encoded_image.jpg")
+    textd=decode(newimage)
+    print(textd)
+  
 
 def createUser():
     pass
